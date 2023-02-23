439dc7857bb4c54f57598d4b4a0d4503ced7921c
HyuntaMansei <jchoi82kor@gmail.com>
2023-02-23 22:37:55 +0900

    20/02/23

--- sql/1.sql
+++ sql/1.sql
@@ -2,37 +2,31 @@ show databases;
 use stock;
 show grants;
 
-drop table if EXISTS STOCK_BASIC_INFO_TB;
-CREATE TABLE STOCK_BASIC_INFO_TB (
-  id INT NOT NULL AUTO_INCREMENT,
-  stock_code varchar(10) not null unique,
-  stock_name VARCHAR(255) null,
-  market_clas varchar(20) null,
-  created_date datetime not null,
-  is_updated char(1) null,
-  updated_date DATETIME,
-  PRIMARY KEY (id)
+drop table if exists STOCK_BASIC_INFO_TB;
+create table STOCK_BASIC_INFO_TB (
+    id int not null auto_increment,
+    stock_code varchar(10) not null unique,
+    stock_name varchar(255) null,
+    market_clas varchar(20) null,
+    created_date datetime not null,
+    is_updated char(1) null,
+    updated_date datetime,
+    primary key (id)
 );
 
-alter table STOCK_BASIC_INFO_TB add column market_clas varchar(20) default NULL;
-alter table STOCK_BASIC_INFO_TB 
+alter table STOCK_BASIC_INFO_TB add column market_clas varchar(20) default null;
+alter table STOCK_BASIC_INFO_TB
 
-MODIFY market_clas varchar(20)
+modify market_clas varchar(20)
 after stock_name;
 
-insert into STOCK_BASIC_INFO_TB (stock_code, stock_name, market_clas, created_date) values ('123456', '메롱메롱메롱', 'ETF', NOW()) as (a, b, c, d)
-on DUPLICATE KEY UPDATE stock_code = c;
+insert into STOCK_BASIC_INFO_TB (stock_code, stock_name, market_clas, created_date)
+values ('123456', '메롱메롱메롱', 'etf', now())
+on duplicate key update stock_name = values(stock_name), market_clas = values(market_clas), is_updated = 'y', updated_date = now();
 
-insert into STOCK_BASIC_INFO_TB (stock_code, stock_name, market_clas, created_date) values ('123456', '메롱메롱메롱', 'ETF', NOW() as y)
-on DUPLICATE KEY UPDATE stock_code=0;
+drop table if exists STOCK_BASIC_INFO_TB;
 
-insert into STOCK_BASIC_INFO_TB 
-(stock_code, stock_name, market_clas, created_date) 
-values ('123456', '메롱메롱메롱', 'ETF', NOW())
-on duplicate key update 
-stock_name = values(stock_name), market_clas = values(market_clas), is_updated = 'y', updated_date = now();
+desc STOCK_BASIC_INFO_TB;
+select * from STOCK_BASIC_INFO_TB where market_clas = 'etf';
 
-drop table if EXISTS STOCK_BASIC_INFO_TB;
-SHOW TABLE STOCK_BASIC_INFO_TB;
-DESC STOCK_BASIC_INFO_TB;
-SELECT * from STOCK_BASIC_INFO_TB;
+delete from STOCK_BASIC_INFO_TB where id=1;
